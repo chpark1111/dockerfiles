6eaf60e6562877da3772b051c795d9fc61978095
Chanhyeok Park <50504528+chpark1111@users.noreply.github.com>
2022-03-13 13:39:57 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -10,6 +10,6 @@ rm /usr/bin/python3;ln -s python3.9 /usr/bin/python3
 pip install --upgrade pip setuptools wheel
 pip install opencv-python
 pip install -r requirements3.9.txt
-pip install torch==1.10.2+cu113 torchvision==0.11.3+cu113 torchaudio==0.10.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
 service ssh start
